54aa0140c2043fb9bd069c27c8db4659d3770a52
@KSGitUser <rusakk@mail.ru>
2021-02-24 19:21:49 +0300

    feat: add towel-sort for matrix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let temp = [];
+  const sortFunction = (a, b, index) => (index + 1) % 2 > 0 ? a - b : b - a; 
+  (matrix || []).map(
+    (arr, index) => temp = [...temp, ...arr.sort((a,b) => sortFunction(a,b,index))]
+    );
+  return temp;
 }
